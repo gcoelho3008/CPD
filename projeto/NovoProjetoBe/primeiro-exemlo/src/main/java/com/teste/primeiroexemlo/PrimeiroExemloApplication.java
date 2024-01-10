74cdecedb68bd15af860f617747da11459dbb6f0
gcoelho3008 <133285197+gcoelho3008@users.noreply.github.com>
2024-01-10 13:26:48 -0300

    Projeto  front-end usnado html css reactjs
    Aplicação consiste em uma página com interações de comentários, comentando, editando e, excluindo.
    Na text area foi aplicado uma função para que não seja feito comentário vazio

---
+++ projeto/NovoProjetoBe/primeiro-exemlo/src/main/java/com/teste/primeiroexemlo/PrimeiroExemloApplication.java
@@ -0,0 +1,13 @@
+package com.teste.primeiroexemlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroExemloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroExemloApplication.class, args);
+	}
+
+}
---
+++ projeto/NovoProjetoBe/primeiro-exemlo/src/test/java/com/teste/primeiroexemlo/PrimeiroExemloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.primeiroexemlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroExemloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
